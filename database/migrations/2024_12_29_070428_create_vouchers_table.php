7b1d14969b71aafc0a6047bf5e0906a80a58f4d8
ApliCalvin <thecalvins.sims@gmail.com>
2024-12-29 14:50:36 +0700

    First Init: - Initialize Database - Initialize Models - Initialize awal
    by ApliCalvin at 29/12/24 at 14:50

---
+++ database/migrations/2024_12_29_070428_create_vouchers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->integer('point_required');
+            $table->decimal('discount',10,2)->default(0);
+            $table->date('expired_date')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vouchers');
+    }
+};
