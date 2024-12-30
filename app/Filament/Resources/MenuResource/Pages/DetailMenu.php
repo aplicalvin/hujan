2355757548bcf81eb670646b97bf637887977d8d
yordanluturyalii <yordanluturyali@gmail.com>
2024-12-30 23:02:06 +0700

    refactor: update detail menu :recycle:

--- app/Filament/Resources/MenuResource/Pages/DetailMenu.php
+++ app/Filament/Resources/MenuResource/Pages/DetailMenu.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\MenuResource\Pages;
 use App\Filament\Resources\MenuResource;
 use Filament\Actions;
 use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\Section;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Pages\ViewRecord;
@@ -17,12 +18,14 @@ class DetailMenu extends ViewRecord
     public function infolist(Infolist $infolist): Infolist
     {
         return $infolist->schema([
-            TextEntry::make('category.name'),
-            TextEntry::make('name'),
-            TextEntry::make('description'),
-            TextEntry::make('price'),
-            ImageEntry::make('image'),
-            TextEntry::make('point')
+            Section::make('Detail Menu')->schema([
+                TextEntry::make('category.name'),
+                TextEntry::make('name'),
+                TextEntry::make('description'),
+                TextEntry::make('price')->money("IDR", 1, "id"),
+                ImageEntry::make('image'),
+                TextEntry::make('point')
+            ])
         ]);
     }
 }
