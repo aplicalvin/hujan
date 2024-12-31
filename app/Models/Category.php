737a423bfebf1bddd7071d881637a61cb08e0cd9
ApliCalvin <thecalvins.sims@gmail.com>
2024-12-31 13:45:45 +0700

    modify beberapa model

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function menus() {
+        return $this->hasMany(Menu::class);
+    }
 }
--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Menu extends Model
 {
-    //
+    protected $guarded = ['id'];
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
 }
